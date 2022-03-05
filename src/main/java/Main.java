f2a7bc5a2b9dafc0099f6bdb548f39c44bc4234e
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2022-03-05 14:02:42 +0100

    add outputs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,16 @@ public class Main {
       String sql = args[0];
       String output = args[1];
       String filename = "";
+      System.out.println("Start executing " + sql + "...");
       if (output != null && output.contains(".")) {
         filename = output.substring(output.lastIndexOf(".") + 1);
+      } else {
+        System.out.println(
+            "No file format detected, verify your file path (need to be a bmp picture)");
       }
       SqlPicQueryParser sqlPicQueryParser = new SqlPicQueryParser();
       sqlPicQueryParser.parseToWriteImage(sql, output, filename);
+      System.out.println("Query executed ! Output picture : " + output);
     } else {
       System.out.println("Use like this : picsql.kar \"sql_query\" \"output_file_path\"");
     }
